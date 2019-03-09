7771088aeb646a22765cac91ad99e30eb168cb54
Vivek Saini <vivek.saini@mail.utoronto.ca>
2019-03-09 17:43:58 -0500

    added logic until before solving
    added the logic to the game up until the actual solve step ye i swear i speak good english

--- Sudoku/SudokuBoard.cpp
+++ Sudoku/SudokuBoard.cpp
@@ -12,12 +12,12 @@ SudokuBoard::SudokuBoard()
 			board[i][j] = 0;
 		}
 	}
+	emptySpaces = 81;
 }
 
 // parses sudoku board assuming qqwing format
-SudokuBoard::SudokuBoard(const char * str)
+SudokuBoard::SudokuBoard(const char * str) : SudokuBoard()
 {
-	SudokuBoard();
 	// go through the string and construct the board from it
 	// if a character in the string is a dot, the board is empty there
 	// assumes 81 characters in string
@@ -26,12 +26,13 @@ SudokuBoard::SudokuBoard(const char * str)
 			if (str[i * 9 + j] == '.') {
 				board[i][j] = 0;
 			} else {
-				board[i][j] = str[i * 9 + j] - '0';
+				insert(i, j, str[i * 9 + j] - '0');
 			}
 		}
 	}
 }
 
+// do nothing
 SudokuBoard::~SudokuBoard()
 {
 }
@@ -51,6 +52,12 @@ inline const bool& SudokuBoard::checkInsertIntoBox(unsigned box, unsigned char v
 	return boxValidityChecker[box][val - 1];
 }
 
+// the above but it takes a row and col as arguments and deduces the box
+inline const bool& SudokuBoard::checkInsertIntoBox(unsigned row, unsigned col, unsigned char val) {
+	unsigned box = (row / 3) * 3 + (col / 3);
+	return boxValidityChecker[box][val - 1];
+}
+
 // check that an insertion given row is valid
 inline const bool& SudokuBoard::checkInsertIntoRow(unsigned row, unsigned char val) {
 	return boxValidityChecker[row][val - 1];
@@ -59,6 +66,37 @@ inline const bool& SudokuBoard::checkInsertIntoRow(unsigned row, unsigned char v
 // check that an insertion given col is valid
 inline const bool& SudokuBoard::checkInsertIntoColumn(unsigned col, unsigned char val) {
 	return boxValidityChecker[col][val - 1];
+}
+
+// sets a cell to be invalid
+// used after insertion
+inline void SudokuBoard::setValidity(unsigned row, unsigned col, unsigned box, unsigned val) {
+	boxValidityChecker[box][val - 1] = rowValidityChecker[row][val - 1] = colValidityChecker[col][val - 1] = false;
+}
+
+// returns a vector of valid options to enter into a given cell
+std::vector<unsigned char> SudokuBoard::getValidValsForCell(unsigned row, unsigned col) {
+	unsigned box = (row / 3) * 3 + (col / 3);
+	// first get the correct array for the row, col, and box
+	bool * cellValidityRow = rowValidityChecker[row];
+	bool * cellValidityCol = colValidityChecker[col];
+	bool * cellValidityBox = boxValidityChecker[box];
+	// now go thorugh and construct the vector from the elements that are valid in the row, col, and box
+	std::vector<unsigned char> cellValidValues;
+	for (unsigned char i = 0; i < 9; i++) {
+		if (cellValidityBox[i] && cellValidityCol[i] && cellValidityRow[i]) {
+			cellValidValues.push_back(i);
+		}
+	}
+	return cellValidValues;
+}
+
+// sets a cell to be valid
+// used before removal of a cell from the board
+inline void SudokuBoard::unsetValidity(unsigned row, unsigned col) {
+	unsigned box = (row / 3) * 3 + (col / 3);
+	unsigned val = board[row][col];
+	boxValidityChecker[box][val - 1] = rowValidityChecker[row][val - 1] = colValidityChecker[col][val - 1] = true;
 }
 
 // do the checks above and then insert
@@ -71,7 +109,47 @@ bool SudokuBoard::insert(unsigned row, unsigned col, unsigned char val) {
 	// otherwise insert and update tables as neccessary
 	} else {
 		board[row][col] = val;
-		boxValidityChecker[box][val - 1] = rowValidityChecker[row][val - 1] = colValidityChecker[col][val - 1] = false;
+		setValidity(row, col, box, val);
 		return true;
 	}
+	// we have one less empty space
+	emptySpaces--;
+}
+
+// remove a value from the cell from a given row and col
+void SudokuBoard::remove(unsigned row, unsigned col) {
+	// unsets the validity tables
+	unsetValidity(row, col);
+	// change the cell state back to empty (denoted by a 0)
+	board[row][col] = 0;
+	// we have one more empty space
+	emptySpaces++;
+}
+
+// actually solve the game
+// i.e. what this program was made for
+void SudokuBoard::solve() {
+
+}
+
+// overloads the << operator so you can pipe the class to a stream
+// and print in a fancy way
+std::ostream& operator<< (std::ostream& os, const SudokuBoard& sb) {
+	for (int i = 0; i < 9; i++) {
+		if (i == 3 || i == 6) {
+			os << "-------|-------|-------" << std::endl;
+		}
+		for (int j = 0; j < 9; j++) {
+			if (j == 3 || j == 6) {
+				os << " |";
+			}
+			if (sb.board[i][j] == 0) {
+				os << " .";
+			} else {
+				os << " " << +sb.board[i][j];
+			}
+		}
+		os << std::endl;
+	}
+	return os;
 }
--- Sudoku/SudokuBoard.h
+++ Sudoku/SudokuBoard.h
@@ -1,4 +1,18 @@
 #pragma once
+#include <string>
+#include <stack>
+#include <vector>
+#include <iostream>
+
+// a struct to represent a move
+// consists of a row and col for the location
+// and the value in that location
+struct Move {
+	unsigned char row;
+	unsigned char col;
+	unsigned char val;
+};
+
 // this is the class that holds the Sudoku Board
 // should also have methods to check for validity
 class SudokuBoard
@@ -9,7 +23,6 @@ public:
 	// parses the board assuming the string is generated from QQWing Sudoku
 	SudokuBoard(const char * board);
 
-
 	// check that a given row/column/box is valid
 	// probably not all that useful
 	bool checkRow(unsigned row);
@@ -17,13 +30,30 @@ public:
 	bool checkBox(unsigned box);
 
 	// checks that inserting into a given row/col/box is valid
-	const bool& checkInsertIntoRow(unsigned row, unsigned char val);
-	const bool& checkInsertIntoColumn(unsigned col, unsigned char val);
-	const bool& checkInsertIntoBox(unsigned box, unsigned char val);
+	inline const bool& checkInsertIntoRow(unsigned row, unsigned char val);
+	inline const bool& checkInsertIntoColumn(unsigned col, unsigned char val);
+	inline const bool& checkInsertIntoBox(unsigned box, unsigned char val);
+	inline const bool& checkInsertIntoBox(unsigned row, unsigned col, unsigned char val);
+
+	// returns all of the valid values that can be entered in a cell without
+	// disrupting the row, column, and box invariants in sudoku
+	std::vector<unsigned char> getValidValsForCell(unsigned row, unsigned col);
+
+	// sets and unsets the validity of a cell
+	inline void setValidity(unsigned row, unsigned col, unsigned box, unsigned val);
+	inline void unsetValidity(unsigned row, unsigned col);
 
 	// insert into the cell after doing the checks
 	bool insert(unsigned row, unsigned col, unsigned char val);
 
+	// remove a value from a cell und update the validity tables
+	void remove(unsigned row, unsigned col);
+
+	// overload the << operator to print
+	friend std::ostream& operator<< (std::ostream& os, const SudokuBoard& sb);
+
+	// solves the sudoku puzzle
+	void solve();
 private:
 	// store the board
 	unsigned char board[9][9];
@@ -33,6 +63,11 @@ private:
 	bool rowValidityChecker[9][9];
 	bool colValidityChecker[9][9];
 	bool boxValidityChecker[9][9];
+	// stacks to keep track of moves that have been made and moves that are yet to be made
+	std::stack<Move> movePossiblities;
+	std::stack<Move> moveTrace;
+	// keeps track of how many moves remain
+	short emptySpaces;
 };
 
 
